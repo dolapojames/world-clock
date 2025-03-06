4a230ba8d0b8d7e0515363cea36ef85aba3d6cd6
dolapojames <oludolapo@hotmail.com>
2025-03-06 10:24:24 +0000

    Added initial moment library and created initial Javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  //London
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
 
+  londonDateElement.innerHTML = moment().format("MMMM D YYYY");
+  londonTimeElement.innerHTML = moment
+    .tz("Europe/London")
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  //New York
+  let newyorkElement = document.querySelector("#new-york");
+  let newyorkDateElement = newyorkElement.querySelector(".date");
+  let newyorkTimeElement = newyorkElement.querySelector(".time");
+
+  newyorkDateElement.innerHTML = moment().format("MMMM D YYYY");
+  newyorkTimeElement.innerHTML = moment
+    .tz("America/New_York")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
